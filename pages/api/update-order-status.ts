b1b46b6625e295f2ad048a3302f56c2360fae8bf
DoHyunHwang <dohyun033@gmail.com>
2023-07-24 19:57:27 +0900

    fix: remove not used
    remove not used

--- pages/api/update-order-status.ts
+++ pages/api/update-order-status.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Cart, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { authOptions } from './auth/[...nextauth]';
 
